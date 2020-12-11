8943bd4b491467cced46c8c01a55b7a232467963
ii02735 <yadallee.bilaal@outlook.fr>
2020-12-11 20:26:53 +0100

    new action

--- index.js
+++ index.js
@@ -6,4 +6,13 @@ app.get("/",(req,res) => {
 	return res.json({"hello":"world"})
 })
 
+app.get("/books",(req,res) => {
+	fs.readFile("json_files/file.json",(err,data) => {
+            if(err)
+		res.json({"message":"no books !"})
+	    else
+		res.json(JSON.parse(data))
+	})
+})
+
 app.listen(process.env.PORT || 3000)
